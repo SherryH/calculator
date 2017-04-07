ae4e3a82fe65b2928944fcb9ccbeba20a5a989bd
Sherry Hsu <sherry.hsu@orionhealth.com>
2017-04-08 11:36:56 +1200

    Changed calculator into a stateful component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div className="calc">
-      A calculator
-      <input className="panel" />
-      <div className="keypad">
-        <div className="row">
-          <div className="col btn ">7</div>
-          <div className="col btn ">8</div>
-          <div className="col btn ">9</div>
-          <div className="col btn blue ">x</div>
-          <div className="col btn blue ">/</div>
-        </div>
-        <div className="row">
-          <div className="col btn ">4</div>
-          <div className="col btn ">5</div>
-          <div className="col btn ">6</div>
-          <div className="col btn blue ">+</div>
-          <div className="col btn blue ">-</div>
-        </div>
-        <div className="row">
-          <div className="col btn ">1</div>
-          <div className="col btn ">2</div>
-          <div className="col btn ">3</div>
-          <div className="col btn blue ">=</div>
-          <div className="col btn blue ">C</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="calc">
+        A calculator
+        <input className="panel" />
+        <div className="keypad">
+          <div className="row">
+            <div className="col btn ">7</div>
+            <div className="col btn ">8</div>
+            <div className="col btn ">9</div>
+            <div className="col btn blue ">x</div>
+            <div className="col btn blue ">/</div>
+          </div>
+          <div className="row">
+            <div className="col btn ">4</div>
+            <div className="col btn ">5</div>
+            <div className="col btn ">6</div>
+            <div className="col btn blue ">+</div>
+            <div className="col btn blue ">-</div>
+          </div>
+          <div className="row">
+            <div className="col btn ">1</div>
+            <div className="col btn ">2</div>
+            <div className="col btn ">3</div>
+            <div className="col btn blue ">=</div>
+            <div className="col btn blue ">C</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default App;
